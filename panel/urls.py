63d35ea4c64847e038dd593db3bb2d6493d13601
mrblacyk <kweinzettl@gmail.com>
2020-01-23 22:19:07 +0100

    Working machine AJAX request control

--- panel/urls.py
+++ panel/urls.py
@@ -3,7 +3,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Dashboards
     path('', views.index, name='index'),
+    path('news/', views.news, name='news'),
+
+    # Machine related URLs
+    path('machines/', views.machines, name='machines'),
+    path('machines/start/<int:machine_id>/', views.start_machine,
+         name='start_machine'),
+    path('machines/stop/<int:machine_id>/', views.stop_machine,
+         name='stop_machine'),
+    path('machines/reset/<int:machine_id>/', views.reset_machine,
+         name='reset_machine'),
+    path('machines/flag/<int:machine_id>/', views.send_flag,
+         name='send_flag'),
+
+    # User handling URLs
     path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
 ]
--- panel/views.py
+++ panel/views.py
@@ -3,13 +3,107 @@ from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.http import HttpResponse
+from time import sleep
 
 # Create your views here.
 
 
 @login_required
 def index(request):
-    return render(request, "panel/index.html", {'username': 'h4wky'})
+    context = {}
+    context.update({'username': request.user.username})
+
+    return render(request, "panel/statistics.html", context)
+
+
+@login_required
+def news(request):
+    context = {}
+    context.update({'username': request.user.username})
+
+    return render(request, "panel/news.html", context)
+
+
+@login_required
+def start_machine(request, machine_id):
+    sleep(1)
+    return HttpResponse(status=400)
+
+
+@login_required
+def stop_machine(request, machine_id):
+    sleep(1)
+    return HttpResponse(status=400)
+
+
+@login_required
+def reset_machine(request, machine_id):
+    sleep(1)
+    return HttpResponse(status=400)
+
+
+@login_required
+def send_flag(request, machine_id):
+    sleep(1)
+    return HttpResponse(status=400)
+
+
+@login_required
+def machines(request):
+    context = {}
+    context.update({'username': request.user.username})
+    context.update({'machines': []})
+    context['machines'].append({
+        'name': 'First',
+        'level': 'Easy',
+        'status': 'RUNNING',
+        'publish_date': '01.01.2020',
+        'user': False, 'root': False,
+        'id': 1,
+    })
+    context['machines'].append({
+        'name': 'Second',
+        'level': 'Medium',
+        'status': 'STOPPED',
+        'publish_date': '01.01.2020',
+        'user': True, 'root': False,
+        'id': 2,
+    })
+    context['machines'].append({
+        'name': 'Third',
+        'level': 'Easy',
+        'status': 'STOPPED',
+        'publish_date': '01.01.2020',
+        'user': True, 'root': True,
+        'id': 3,
+    })
+    context['machines'].append({
+        'name': 'Fourth',
+        'level': 'Hard',
+        'status': 'RUNNING',
+        'publish_date': '01.01.2020',
+        'user': False, 'root': True,
+        'id': 4,
+    })
+    context['machines'].append({
+        'name': 'Fifth',
+        'level': 'Easy',
+        'status': 'SCHEDULED FOR A RESET',
+        'publish_date': '01.01.2020',
+        'user': False, 'root': False,
+        'id': 5,
+    })
+    context['machines'].append({
+        'name': 'Sixth',
+        'level': 'Medium',
+        'status': 'BROKEN BY KURASKOV',
+        'publish_date': '01.01.2020',
+        'user': True, 'root': False,
+        'id': 6,
+    })
+
+    return render(request, "panel/machines.html", context)
 
 
 def login_view(request):
@@ -24,6 +118,14 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
+                messages.success(request, "Logged in!")
+                messages.warning(request, "Warning!")
+                messages.warning(request, "Warning!")
+                messages.warning(request, "Warning!")
+                messages.error(request, "Error!")
+                messages.warning(request, "Warning!")
+                messages.success(request, "Logged in!")
+                messages.success(request, "Logged in!")
                 return redirect('index')
             else:
                 messages.error(request, "Invalid username or password")
