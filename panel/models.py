aedeccd641afc54224fd65d01b340b8f14fa9e57
mrblacyk <kweinzettl@gmail.com>
2020-01-24 11:30:28 +0100

    Fixed messages model and migrations done

--- panel/models.py
+++ panel/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.contrib.auth import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Messages(models.Model):
-    sender = models.ForeignKey(User, related_name="sender")
-    receiver = models.ForeignKey(User, related_name="receiver")
+    sender = models.ForeignKey(
+        User, related_name="sender", on_delete=models.SET_NULL, null=True
+    )
+    receiver = models.ForeignKey(
+        User, related_name="receiver", on_delete=models.SET_NULL, null=True
+    )
     content = models.TextField()
     created_at = models.DateField()
     read = models.BooleanField(default=False)
