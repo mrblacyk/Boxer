ecd92f04012036dc0f47c269d415a25142cd3bd4
mrblacyk <kweinzettl@gmail.com>
2020-01-24 11:25:32 +0100

    Messages model

--- panel/admin.py
+++ panel/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Messages
 
 # Register your models here.
+
+admin.site.register(Messages)
--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
+
+
+class Messages(models.Model):
+    sender = models.ForeignKey(User, related_name="sender")
+    receiver = models.ForeignKey(User, related_name="receiver")
+    content = models.TextField()
+    created_at = models.DateField()
+    read = models.BooleanField(default=False)
